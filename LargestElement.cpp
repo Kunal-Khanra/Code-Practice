f83f22d24816caa10806517715030de6267e9ef2
Kunal Khanra <kunalkhanra@Kunals-MacBook-Air.local>
2026-03-19 19:18:25 +0530

    The Largest element code

---
+++ LargestElement.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<math.h>
+using namespace std;
+
+
+int main()
+{
+    // Take the size of the array
+    int n; cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0 ; i < n ; i += 1) cin >> arr[i];
+
+    // Let the first element be the maximum element
+    int max_element = arr[0];
+    for (int i = 1 ; i < n ; i += 1) 
+        max_element = max(max_element, arr[i]); // Update the maximum element if the current element is greater
+    
+    cout << "The largest element is: " << max_element << endl;
+    return 0;
+}
